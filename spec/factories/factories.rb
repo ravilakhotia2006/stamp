11bcdea741340552ff21377ffd80aa0009d6da9f
ravilakhotia2006 <ravilakhotia2006@gmail.com>
2017-09-01 12:00:05 +0530

    added factories for medical records, user and prescription

---
+++ spec/factories/factories.rb
@@ -0,0 +1,21 @@
+FactoryGirl.define do
+  factory :user do
+    email { Faker::Internet.email }
+    name { Faker::Lorem.word }
+
+    after(:create) do |user|
+      FactoryGirl.create(:medical_record, user: user)
+    end
+  end
+
+  factory :medical_record do
+    after(:create) do |medical_record|
+      FactoryGirl.create(:prescription, medical_record: medical_record)
+    end
+  end
+
+  factory :prescription do
+    url { Faker::Internet.url }
+    description { Faker::Lorem.sentence }
+  end
+end
